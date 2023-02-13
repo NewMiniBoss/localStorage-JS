9dbd10444592d3a223d1f9352bce0cfe268f3a3b
gabriel <gabriel.f.lazari@gmail.com>
2023-02-13 15:46:44 -0300

    complete mission

--- js/main.js
+++ js/main.js
@@ -11,10 +11,28 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements.nome;
     const quantidade = evento.target.elements.quantidade;
     const itensAtuais = { "nome": nome.value, "quantidade": quantidade.value };
-    armazenandoElemento(itensAtuais);
-    criaElemento(itensAtuais);
+    verificaItem(itensAtuais);
     limpaFormulario(nome, quantidade);
 })
+
+function verificaItem(item) {
+    const existe = itens.find(elemento => elemento.nome === item.nome);
+    if (existe) {
+        item.id = existe.id;
+        atualizeElemento(item);
+    } else {
+        item.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0;
+        criaElemento(item);
+        armazenandoElemento(item);
+    }
+}
+
+function atualizeElemento(item) {
+    const elementoId = document.querySelector("[data-id='" + item.id + "']");
+    elementoId.innerHTML = item.quantidade;
+    itens[itens.findIndex(elemento => elemento.id == item.id)] = item;
+    localStorage.setItem('item', JSON.stringify(itens));
+}
 
 function armazenandoElemento(item) {
     itens.push(item);
@@ -24,11 +42,28 @@ function armazenandoElemento(item) {
 function criaElemento(item) {
     const novoUl = document.createElement('li');
     const novoStrong = document.createElement('strong');
+    novoStrong.dataset.id = item.id;
     novoUl.classList.add('item');
     novoUl.appendChild(novoStrong);
     lista.appendChild(novoUl);
     novoStrong.innerHTML = item.quantidade;
     novoUl.innerHTML += item.nome;
+    novoUl.appendChild(btnDelete(item.id));
+}
+
+function btnDelete(id) {
+    const button = document.createElement('button');
+    button.innerHTML = 'X';
+    button.addEventListener('click', function () {
+        deletaElemento(this.parentNode, id);
+    })
+    return button;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);   
+    localStorage.setItem('item', JSON.stringify(itens));
 }
 
 function limpaFormulario(nome, quantidade) {
