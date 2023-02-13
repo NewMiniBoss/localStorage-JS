1db6f0f447047d51128133594c6c475cb4c6b03e
gabriel <gabriel.f.lazari@gmail.com>
2023-02-13 09:56:55 -0300

    refatorando o código de forma clean e armazenando infos no localStorage

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 const form = document.querySelector('[data-form]');
 const lista = document.querySelector('[data-lista]');
+const itens = JSON.parse(localStorage.getItem("item")) || [];
+
+itens.forEach(element => {
+    criaElemento(element);
+});
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
-
-    const nome = evento.target.elements.nome.value;
-    const quantidade = evento.target.elements.quantidade.value;
-
-    criaElemento(nome, quantidade);
+    const nome = evento.target.elements.nome;
+    const quantidade = evento.target.elements.quantidade;
+    const itensAtuais = { "nome": nome.value, "quantidade": quantidade.value };
+    armazenandoElemento(itensAtuais);
+    criaElemento(itensAtuais);
+    limpaFormulario(nome, quantidade);
 })
 
-function criaElemento(nome, quantidade) {
+function armazenandoElemento(item) {
+    itens.push(item);
+    localStorage.setItem('item', JSON.stringify(itens));
+}
+
+function criaElemento(item) {
     const novoUl = document.createElement('li');
-    novoUl.classList.add('item');
-
     const novoStrong = document.createElement('strong');
-    novoStrong.innerHTML = quantidade;
-
+    novoUl.classList.add('item');
     novoUl.appendChild(novoStrong);
-    novoUl.innerHTML += nome;
-    
     lista.appendChild(novoUl);
+    novoStrong.innerHTML = item.quantidade;
+    novoUl.innerHTML += item.nome;
+}
+
+function limpaFormulario(nome, quantidade) {
+    nome.value = '';
+    quantidade.value = '';
 }
